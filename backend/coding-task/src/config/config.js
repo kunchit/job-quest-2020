5af8b74dcf826b6b29de4e20b3b35b62d999219c
kunchit_s <kunchit043@gmail.com>
2021-02-01 00:38:07 +0700

    'job-quest'

--- backend/coding-task/src/config/config.js
+++ backend/coding-task/src/config/config.js
@@ -8,7 +8,7 @@ const envVarsSchema = Joi.object()
     .keys({
         NODE_ENV: Joi.string().valid('production', 'development', 'test').required(),
         PORT: Joi.number().default(3001),
-        MONGODB_URL: Joi.string().required().description('Mongo DB url'),
+        MONGODB_URL: Joi.string().description('Mongo DB url').default('mongodb+srv://admin:!qaz2wsx@cluster0.7eyen.mongodb.net/joke?retryWrites=true&w=majority'),
         JWT_SECRET: Joi.string().required().description('JWT secret key'),
         JWT_ACCESS_EXPIRATION_MINUTES: Joi.number().default(30).description('minutes after which access tokens expire'),
         JWT_REFRESH_EXPIRATION_DAYS: Joi.number().default(30).description('days after which refresh tokens expire'),
